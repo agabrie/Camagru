24fe3a9d58277d338e59f5df85af6f77422b5d90
abduraghmaan GABRIELS <agabrie@c1r2s3.wethinkcode.co.za>
2018-11-01 15:35:29 +0200

    setting up database

--- install.php
+++ install.php
@@ -1,18 +1,38 @@
 <?php
     $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    try {
-        $conn = new PDO("mysql:host=$servername;", $username, $password);
-    // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "CREATE DATABASE CAMAGRU";
-        $conn->exec($sql);
-        echo "Connected and created database successfully"; 
-        }
-    catch(PDOException $e)
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    if(isset($_POST["btn"]))
     {
-        echo "Connection failed: " . $e->getMessage();
+         try {
+            $conn = new PDO("mysql:host=$servername;", $username, $password);
+        //  set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "CREATE DATABASE CAMAGRU";
+            $conn->exec($sql);
+            echo "Connected and created database successfully";
+            // header("Location: server_databases.php");
+          }
+        catch(PDOException $e)
+        {
+          echo "Connection failed: " . $e->getMessage();
+        }
+        $conn = null;
     }
-    $conn = null;
 ?>
+<html>
+    <head>
+        <title>
+            PHP myAdmin Login
+        </title>
+    </head>
+    <body>
+        <form action="" method="post">
+        <label for="username">USERNAME:</label><br>
+            <input type="text" name="username" value="root"><br>
+            <label for="password">PASSWORD:</label><br>
+            <input type="password" name="password" value=""><br>
+            <input type="submit" name="btn" value="login">
+        </form>
+    </body>
+</html>
