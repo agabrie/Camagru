fc1ea500f884d0a58e7643822623d90c176f620b
abduraghmaan GABRIELS <agabrie@c1r2s3.wethinkcode.co.za>
2018-11-05 10:51:33 +0200

    can store user info

--- confirm_login.php
+++ confirm_login.php
@@ -1,7 +1,13 @@
 <?php
     session_start();
-    //echo "something";
-    //print_r($_SESSION);
+    include_once("index.php");
+
     if($_SESSION["username"] != NULL)
         echo $_SESSION["username"]." has loginned!".PHP_EOL;
+    $db->insertRecord(array(    "table"     =>array("name"=>"USERS",
+                                "columns"   =>array("username","fname","lname","email","password","verified", "gallery")
+                                                ),
+                                "values"    =>array("\"".$_SESSION["username"]."\"","\"".$_SESSION["fname"]."\"","\"".$_SESSION["lname"]."\"","\"".$_SESSION["email"]."\"","\"".$_SESSION["passwrd"]."\"", '1', '1')
+                            ));
+    // header("Location: index.php");
 ?>
--- install.php
+++ install.php
@@ -14,18 +14,20 @@ class Db
 		$this->username = $database["username"];
 		$this->password = $database["password"];
 		$this->dbname = $database["dbname"];
+		$this->sconn = new PDO("mysql:host=$this->servername;", $this->username, $this->password);
+		$this->sconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this->createDB();
+		$this->dbconn = new PDO("mysql:host=$this->servername;dbname=$this->dbname;", $this->username, $this->password);
+		$this->dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		echo "db object contructed<br>";	
 	}
 	
 	function createDB()
 	{
 		 try
-		 {
-			$this->sconn = new PDO("mysql:host=$this->servername;", $this->username, $this->password);
-			$this->sconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+		 {	
 			$this->runStatement($this->sconn,"CREATE DATABASE IF NOT EXISTS ".$this->dbname);
-			echo "db created succesfully <br>";
+			echo "db created succesfully <br>";	
 		 }
 		 catch(PDOException $e)
 		 {
@@ -37,8 +39,6 @@ class Db
 	{
 		try
 		{
-			$this->dbconn = new PDO("mysql:host=$this->servername;dbname=$this->dbname;", $this->username, $this->password);
-			$this->dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$statement = "CREATE TABLE  IF NOT EXISTS ".$table["name"];
 			$statement = $statement."(";
 			$count = 0;
@@ -77,39 +77,35 @@ class Db
 	
 	function closeConnections()
 	{
-		$this->sconn = null;
-		$this->dbconn = null;
+		$this->sconn	= null;
+		$this->dbconn	= null;
+	}
+	function insertRecord($record)
+	{
+		$count 		= 0;
+		$statement 	= "INSERT INTO ".$record["table"]["name"];
+		$statement 	= $statement."(";
+		foreach($record["table"]["columns"] as $column)
+		{
+			if($count != 0)
+				$statement = $statement.",".$column;
+			else
+				$statement = $statement.$column;
+			$count++;
+		}
+		$count 		= 0;
+		$statement = $statement.") VALUES (";
+		foreach($record['values'] as $values)
+		{
+			if($count != 0)
+				$statement = $statement.",".$values;
+			else
+				$statement = $statement.$values;
+			$count++;
+		}
+		$statement 	= $statement.");";
+		echo $statement.PHP_EOL;
+		$this->runStatement($this->dbconn, $statement);
 	}
 }
-
-$db = new Db(array("servername"=>"localhost", "username"=>"root","password"=>"R00t","dbname"=>"CAMAGRU"));
-$db->createDB();
-$db->createTABLE(array(	"name"=>"USERS",
-						"columns"=>array(	"username VARCHAR(20) not NULL PRIMARY KEY",
-											"fname VARCHAR(20) default 'Mohammed'",
-											"lname VARCHAR(20) default 'LastNams'",
-											"email VARCHAR(40) not NULL",
-											"`password` VARCHAR(100) not NULL",
-											"verified TINYINT(1) NOT NULL DEFAULT '0'",
-											"gallery INT(11) NOT NULL default '0'"
-										)
-						));
-
-// $db->closeConnnections();
 ?>
-<!-- <html>
-	<head>
-		<title>
-			PHP myAdmin Login
-		</title>
-	</head>
-	<body>
-		<form action="" method="post">
-		<label for="username">USERNAME:</label><br>
-			<input type="text" name="username" value="root"><br>
-			<label for="password">PASSWORD:</label><br>
-			<input type="password" name="password" value=""><br>
-			<input type="submit" name="btn" value="login">
-		</form>
-	</body>
-</html> -->
